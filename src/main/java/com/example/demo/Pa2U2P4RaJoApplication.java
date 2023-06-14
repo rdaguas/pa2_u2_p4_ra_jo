aff10086ad10483d635aee6f47712c88692cda0b
rdaguas <aguas.rdv2001@gmail.com>
2023-06-13 19:44:05 -0500

    Taller16

--- src/main/java/com/example/demo/Pa2U2P4RaJoApplication.java
+++ src/main/java/com/example/demo/Pa2U2P4RaJoApplication.java
@@ -8,16 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.banco.repository.modelo.CuentaBancaria;
-import com.example.demo.banco.service.CuentaService;
-import com.example.demo.repository.modelo.Estudiante;
-import com.example.demo.service.EstudianteService;
-
 @SpringBootApplication
 public class Pa2U2P4RaJoApplication implements CommandLineRunner {
 	
-	@Autowired
-	private CuentaService cuentaService;
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U2P4RaJoApplication.class, args);
@@ -26,15 +20,6 @@ public class Pa2U2P4RaJoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-	CuentaBancaria bancaria = new CuentaBancaria();
-	bancaria.setFechaDeApertura(LocalDateTime.now());
-	bancaria.setNumero("12345");
-	bancaria.setTipo("A");
-	bancaria.setSaldo(new BigDecimal(100));
-	bancaria.setCedulaPropietario("12344566");
-	//this.cuentaService.aperturar(bancaria);
-	System.out.println();
-	this.cuentaService.realizar(1, bancaria.getSaldo());
 	
 	
 	}
